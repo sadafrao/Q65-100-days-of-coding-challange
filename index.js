23511f1967b2456c63a0c5b530b5bbd77fa86956
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:22:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q65
+// thois function show divideing two number 
+function divide(num1, num2) {
+    return num1 % num2;
+}
+console.log(divide(5, 2)); // this is tell us a leftover
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q65
+// thois function show divideing two number 
+function divide(num1: number, num2: number): number {
+    return num1 % num2;
+}
+console.log(divide(5, 2));// this is tell us a leftover
